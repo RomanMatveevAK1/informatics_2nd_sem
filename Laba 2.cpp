a007efcb3ce6f761eddffb75d3e7c8ebcb089012
RomanMatveevAK1 <159236694+RomanMatveevAK1@users.noreply.github.com>
2024-03-12 00:38:29 +0300

    Add files via upload

--- Laba 2.cpp
+++ Laba 2.cpp
@@ -3,12 +3,10 @@
 using namespace std;
 
 void Print_Menu();
-void Input(int cnt);
-
 
 class transport 
 {
-public:
+private:
     int cnt_wheels;
     double mileage;
     double eng_capacity;
@@ -17,95 +15,110 @@ public:
     double consump;
     double massa;
     double time;
+    double len;
     string name;
-    //transport(double val_power, double val_massa);
-    //double calculating_speed();
-};
+public:
+    transport()
+    {
+        int cnt_wheels = 0;
+        double mileage = 0;
+        double eng_capacity = 0;
+        double speed = 0;
+        double power = 0;
+        double consump = 0;
+        double massa = 0;
+        double time = 0;
+        double len = 0;
+        string name = " ";
+        cout << "ТС создано" << endl;
+    }
 
-//transport::transport(double val_power, double val_massa)
-//{
-//    double power = val_power;
-//    double massa = val_massa;
-//}
-//
-//double transport::calculating_speed()
-//{
-//    return 735,5 * power / (0.02 * massa);
-//}
+    void Inprint(int cnt)
+    {
+        for (int i = 0; i < cnt; i++)
+        {
+            cout << "Транспорт №" << i + 1 << endl;
+            cout << "Название ТС: " << endl;
+            cin >> name;
+            cout << "Количество колёс (шт.):" << endl;
+            cin >> cnt_wheels;
+            cout << "Мощность двигателя (л. с.):" << endl;
+            cin >> power;
+            cout << "Объём двигателя (л):" << endl;
+            cin >> eng_capacity;
+            cout << "Пробег (км):" << endl;
+            cin >> mileage;
+            consump = 100 * eng_capacity / mileage;
+            cout << "Потребление двигателя (л/км):" << consump << endl;
+            cout << "Масса (кг):" << endl;
+            cin >> massa;
+            speed = power * 735.5 / (0.02 * massa);
+            cout << "Скорость транспортного средства (м/c):" << speed << endl;
+            cout << "Название: " << name << "\t" << "Количество колёс: " << cnt_wheels << "\t" << "Мощность: " << power << "\t" << "Потребление: " << consump << "\t" << "Скорость: " << speed << endl;
+        }
+    }
+
+    //~transport()
+    //{
+    //    cout << "Тут сработал деструктор!" << endl;
+    //}
+
+    void len_route()
+    {
+        cout << "Введите длину маршрута: " << endl;
+        cin >> len;  
+    }
+    
+    void calc_len_route()
+    {
+        time = len / speed;
+        cout << "Время: " << time << endl;
+    }
+};
 
 void Print_Menu()
 {
-    cout << "--------------------------МЕНЮ--------------------------\n 1. Ввести и посмотреть данные о ТС\n 2. Ввести протяженность трассы\n 3. Произвести расчет прохождения трассы\n 4. Осуществить вывод результатов прохождения трассы\n 5. Выход\n" << endl;
+    cout << "-------------------------||МЕНЮ||--------------------------\n 1. Ввести и посмотреть данные о ТС\n 2. Ввести протяженность трассы\n 3. Произвести расчет прохождения трассы\n 4. Осуществить вывод результатов прохождения трассы\n 5. Выход\n" << endl;
 }
-
-void Input(int cnt)
-{
-    transport* ptr = new transport[cnt];
-    for (int i = 0; i < cnt; i++)
-    {
-        transport TS;
-        cout << "Транспорт №" << i + 1 << endl;
-        cout << "Название ТС: " << endl;
-        cin >> TS.name;
-        cout << "Количество колёс (шт.):" << endl;
-        cin >> TS.cnt_wheels;
-        cout << "Мощность двигателя (л. с.):" << endl;
-        cin >> TS.power;
-        cout << "Объём двигателя (л):" << endl;
-        cin >> TS.eng_capacity;
-        cout << "Пробег (км):" << endl;
-        cin >> TS.mileage;
-        TS.consump = 100 * TS.eng_capacity / TS.mileage;
-        cout << "Потребление двигателя (л/км):" << TS.consump << endl;
-        cout << "Масса (кг):" << endl;
-        cin >> TS.massa;
-        TS.speed = TS.power * 735.5 / (0.02 * TS.massa);
-        cout << "Скорость транспортного средства (м/c):" << TS.speed;
-
-        ptr[i] = TS;
-    }
-    for (int i = 0; i < cnt; i++)
-    {
-        cout << "Transport №" << i + 1 << "\t\t";
-        cout << ptr[i].name << "\t\t";
-        cout << ptr[i].cnt_wheels << "\t\t";
-        cout << ptr[i].power << "\t\t";
-        cout << ptr[i].consump << "\n";
-        cout << ptr[i].speed << endl;
-    }
-}
-
 
 int main()
 {
-
     setlocale(LC_ALL, "ru");
     Print_Menu();
-
     int choose, cnt;
-
     cout << "Выберите номер пункта меню" << endl;
-    cin >> choose;
 
 
-    switch (choose)
+    do
     {
-        case 1:
-            cout << "Ввод данных о транспортных средствах\n" << endl;
+        cin >> choose;
+
+        if (choose == 1)
+        {
+            cout << "Ввод и вывод данных о транспортных средствах (TC)\n" << endl;
             cout << "Введите количество транспортных средств:" << endl;
             cin >> cnt;
-            Input(cnt);
+            transport TS;
+            TS.Inprint(cnt);
             break;
-        case 2:
-
-
-        /*case 4:
-        case 5:
-        case 6:*/
-        default:
-            cout << "Ошибка! Попробуйте сделать выбор еще раз.";
-
-    }
+        }
+        if (choose == 2)
+        {
+            cout << "Ввод данных о протяженности трассы (TC)\n" << endl;
+            transport TS;
+            TS.len_route();
+            break;
+        }
+        if (choose == 3)
+        {
+            cout << "Расчёт протяжённости трассы" << endl;
+            transport TS;
+            TS.calc_len_route();
+            break;
+        }
+        if (choose == 4)
+            system("pause");
+    } while (choose != 4);
 }
 
 
